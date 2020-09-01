7b9bcec333ef72755fff5377676e2e63a8081a06
Teresa Heidt <teresaheidt@hotmail.com>
2020-09-01 15:28:14 -0500

    changed file locations

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,10 +1,11 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
    
-    constructor(name, id, email) {
+    constructor(name, id, email, role) {
       this.name = name;
       this.id = id;
       this.email = email;
+      this.email = role;
     }
 
     getName() {
@@ -18,11 +19,16 @@ class Employee {
     getEmail() {
       return this.email;
     }
+
+    getRole() {
+      return this.email;
+    }
   
     printInfo() {
       console.log(`Name: ${this.name}`);
       console.log(`Id: ${this.id}`);
       console.log(`Email: ${this.email}`);
+      console.log(`Role: ${this.role}`);
     }
   }
   
